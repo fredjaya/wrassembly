2e286116339e30ad9e687f34fe1e36d72e6c51ac
fredjaya <fredjaya1@gmail.com>
2021-06-02 19:57:41 +1000

    PF and FW longranger completed

--- src/longranger_basic.sh
+++ src/longranger_basic.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 #PBS -P RDS-FSC-WholeGenomeAssembly-RW
-#PBS -l select=1:ncpus=4:mem=32GB
+#PBS -l select=1:ncpus=4:mem=64GB
 #PBS -l walltime=12:00:00
 
 LR=/home/fjay0039/longranger-2.2.2/longranger
-DATA=/project/WholeGenomeAssembly/parrotfish
-OUT=/scratch/WholeGenomeAssembly/2105_longranger
+DATA=/scratch/WholeGenomeAssembly/convert_tellseq_10x_4M/FW
+#DATA=/scratch/WholeGenomeAssembly/convert_tellseq_10x_4M/PF
+OUT=/scratch/WholeGenomeAssembly/longranger
 
 cd ${OUT}
-$LR basic --id=PF-4M --fastqs=${DATA}
+#$LR basic --id=PF-4M --fastqs=${DATA}
+$LR basic --id=FW-4M --fastqs=${DATA}
+
