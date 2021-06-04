bd57cdec6694934392d86eeba1d6d23d9a224b4d
fredjaya1 <fredjaya1@gmail.com>
2021-06-04 11:06:40 +1000

    Identified good ARKS/LINKS parameters

--- src/arcs.sh
+++ src/arcs.sh
@@ -6,12 +6,12 @@ OUT=/media/meep/GenomeAbyss/assembly
 export PATH=$PATH:/home/meep/Desktop/Biocomputing/links_v1.8.7
 ID=$1
 
-cd ${OUT}/arks/${ID}
+cd ${OUT}/arcs/${ID}
 
 cp -s ${OUT}/${ID}_canu_purged_arrow.fasta ${ID}_canu_purged_arrow.fa
 cp -s ${OUT}/longranger/${ID}-barcoded.fastq.gz ${ID}-barcoded.fq.gz
 
-${BIN}/Examples/arcs-make arks \
+${BIN}/Examples/arcs-make arcs \
 	draft=${ID}_canu_purged_arrow \
 	reads=${ID}-barcoded \
 	c=3 \
